e78e73960ab6298f3d9b4ce6a1b617044a6a4950
Sedat Polat <sedatpolat355@gmail.com>
2024-09-19 11:35:01 +0300

    color list rendered for mobile view

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,11 +2,27 @@ import BrandsData from "../brands-data.json"
 
 function Content() {
 
-  console.log(BrandsData)
+  const renderedBrands = Object.entries(BrandsData).map(([key, objValue]) => {
+    return(
+      <li className="brand-color-box" key={key}>
+        <span className="brand-name">{key}</span>
+        <ul className="colors">
+          {objValue.colors.map((color, ind) => {
+            return(
+              <li className="color" key={ind} style={{ backgroundColor:`#${color}`, border: `1px solid #${color}` }}>
 
+              </li>
+             ) 
+          })}
+        </ul>
+      </li>
+    )
+  })
   return(
     <div className="content">
-      content
+      <ul className="color-list">
+        {renderedBrands}
+      </ul>
     </div>
   )
 }
