a324ee313aaebb5682ab2f2e21120172531869f6
Sedat Polat <sedatpolat355@gmail.com>
2024-09-19 10:55:11 +0300

    header completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,31 @@
+import { IoMdSearch } from "react-icons/io"
+import { MdDownload } from "react-icons/md"
+import { MdInsertLink } from "react-icons/md"
+import { IoClose } from "react-icons/io5"
+
 function Header() {
-  return(
-    <div className="header">header</div>
+  return (
+    <div className="header">
+      <div className="left-side">
+        <IoMdSearch className="search-icon" />
+        <input 
+          className="search-input" placeholder="Search Brands" />
+      </div>
+
+      <div className="right-side">
+        <div className="brands-collected">
+          <MdDownload className="collected-icon"/>
+          <MdInsertLink className="collected-icon"/>
+          <IoClose className="collected-icon"/>
+          <span>0 brands collected</span>
+        </div>
+
+        <div className="all-brands">
+          <MdDownload className="download-icon" />
+          <span>All Brands</span>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Header
