a366d99cd702c3a80bf970108d254d6284b1cb3c
Sedat Polat <sedatpolat355@gmail.com>
2024-09-20 14:33:11 +0300

    missing elements in sidebar completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,26 @@
-import {useState} from "react"
+import { useEffect, useState } from "react"
 import Modal from "./Modal"
 
 function Sidebar() {
   const [isOpenModal, setIsOpenModal] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
 
- 
   const handleOpenModal = () => {
     setIsOpenModal(true)
   }
 
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth)
+    window.addEventListener("resize", handleResize)
+
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
   return (
     <div className="sidebar">
-      {isOpenModal && <Modal isOpenModal={isOpenModal} setIsOpenModal={setIsOpenModal}/>}
+      {isOpenModal && (
+        <Modal isOpenModal={isOpenModal} setIsOpenModal={setIsOpenModal} />
+      )}
       <div className="logo-container">
         <img
           className="logo-img"
@@ -23,13 +32,30 @@ function Sidebar() {
         </a>
       </div>
 
+      {screenWidth >= 1024 && (
+        <p className="added-parag-1">
+          The biggest collection of official brand color codes around. Curated
+          by <a>@brandcolors</a> and friends.
+        </p>
+      )}
+
       <div className="sidebar-links">
         <div className="text-links">
           <a>Suggest a Brand</a>
-          <a
-            onClick={handleOpenModal}
-          >About BrandColors</a>
+          <a onClick={handleOpenModal}>About BrandColors</a>
         </div>
+
+        {screenWidth >= 1024 && (
+          <div className="added-parag-2">
+            <a>
+              <span>
+                Brand<b>Colors</b> + DesignBombs
+              </span>
+              Learn how to make a website - we have put together an in-depth
+              guide that will help you build your first website with WordPress.
+            </a>
+          </div>
+        )}
 
         <div>
           <a href="https://www.namecheap.com/hosting/shared/?clickID=QKUzuiULPxyPRrBxlP15MXB6UkCy2DxU1xvRTs0&irgwc=1&utm_source=IR&utm_medium=Affiliate&utm_campaign=1939807&affnetwork=ir&ref=ir">
@@ -41,14 +67,22 @@ function Sidebar() {
         </div>
 
         <div>
-          <a className="desc-link" href="https://www.namecheap.com/hosting/shared/?clickID=QKUzuiULPxyPRrBxlP15MXB6UkCy2DU81xvRTs0&irgwc=1&utm_source=IR&utm_medium=Affiliate&utm_campaign=1939807&affnetwork=ir&ref=ir">
+          <a
+            className="desc-link"
+            href="https://www.namecheap.com/hosting/shared/?clickID=QKUzuiULPxyPRrBxlP15MXB6UkCy2DU81xvRTs0&irgwc=1&utm_source=IR&utm_medium=Affiliate&utm_campaign=1939807&affnetwork=ir&ref=ir"
+          >
             Build your website for just $3.88/mth. More value and performance
             with Namecheap
           </a>
         </div>
 
         <div>
-          <a className="ads" href="https://www.carbonads.net/?utm_source=brandcolorsnet&utm_medium=ad_via_link&utm_campaign=in_unit&utm_term=carbon">ads via carbon</a>
+          <a
+            className="ads"
+            href="https://www.carbonads.net/?utm_source=brandcolorsnet&utm_medium=ad_via_link&utm_campaign=in_unit&utm_term=carbon"
+          >
+            ads via carbon
+          </a>
         </div>
       </div>
     </div>
